3da92c2f64564cfbab1d90d0f0ab0c5597ff9a5f
Alex <alexandrughinea.dev+github@gmail.com>
2024-11-13 19:38:53 +0200

    Update production.bru
    Updates

--- src/routes/internal/users.rs
+++ src/routes/internal/users.rs
@@ -11,31 +11,6 @@ pub(crate) fn configure(cfg: &mut web::ServiceConfig) {
         .route("", web::get().to(get_users));
 }
 
-pub async fn get_users(
-    req: HttpRequest,
-    state: web::Data<AppState>,
-    pool: web::Data<PgPool>,
-    configuration: web::Data<Configuration>,
-    web::Query(params): web::Query<FlatQueryParams>,
-) -> HttpResponse {
-    let tenant_id = match get_tenant_id_from_request(&req) {
-        Ok(id) => id,
-        Err(e) => return e,
-    };
-    let tenant_pool = match get_pool_for_tenant(&tenant_id, &state, &pool, &configuration).await {
-        Ok(pool) => pool,
-        Err(e) => return e,
-    };
-
-    let users = paginated_query_as!(User, "SELECT * FROM users")
-        .with_params(params)
-        .fetch_paginated(&tenant_pool)
-        .await
-        .unwrap();
-
-    HttpResponse::Ok().json(json!(users))
-}
-
 pub async fn create_user(
     req: HttpRequest,
     user: web::Json<User>,
@@ -66,4 +41,29 @@ pub async fn create_user(
     .unwrap();
 
     HttpResponse::Ok().json(json!(user))
+}
+
+pub async fn get_users(
+    req: HttpRequest,
+    state: web::Data<AppState>,
+    pool: web::Data<PgPool>,
+    configuration: web::Data<Configuration>,
+    web::Query(params): web::Query<FlatQueryParams>,
+) -> HttpResponse {
+    let tenant_id = match get_tenant_id_from_request(&req) {
+        Ok(id) => id,
+        Err(e) => return e,
+    };
+    let tenant_pool = match get_pool_for_tenant(&tenant_id, &state, &pool, &configuration).await {
+        Ok(pool) => pool,
+        Err(e) => return e,
+    };
+
+    let users = paginated_query_as!(User, "SELECT * FROM users")
+        .with_params(params)
+        .fetch_paginated(&tenant_pool)
+        .await
+        .unwrap();
+
+    HttpResponse::Ok().json(json!(users))
 }
